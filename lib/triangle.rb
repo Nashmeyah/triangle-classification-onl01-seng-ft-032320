be18dc20318e2077df86bf0fcf9e488ff4eb79b1
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-09 02:20:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,18 @@ class Triangle
   end
 
   def kind
-    if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
-      :equilateral
-    elsif side_1 == side_2 && side_2 == side_3 || side_1 == side_3 && side_2 == side_3
-      :isosceles
-    elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
-      :scalene
+    if side_1 > 0 && side_2 > 0 && side_3 > 0
+      if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
+        :equilateral
+      elsif side_1 == side_2 && side_2 == side_3 || side_1 == side_3 && side_2 == side_3
+        :isosceles
+      elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
+        :scalene
+      else
+    else
+      class TriangleError < StandardError
+          # triangle error code
+      end
+    end
   end
 end
