e4fc01735f3f7c5b37a1e684161fc37cd01163f8
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-09 02:13:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,6 @@ class Triangle
     if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
       :equilateral
     elsif side_1 == side_2 && side_2 == side_3 || side_1 == side_3 && side_2 == side_3
-      
+
   end
 end
